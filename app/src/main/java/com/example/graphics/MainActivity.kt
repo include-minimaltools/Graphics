0443ebeb24ce710bdb693bf3940fc31c14e01056
Sawluis <luispineda200504@gmail.com>
2022-03-20 18:46:32 -0600

    Menu

--- app/src/main/java/com/example/graphics/MainActivity.kt
+++ app/src/main/java/com/example/graphics/MainActivity.kt
@@ -7,9 +7,10 @@ import android.view.View
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // setContentView(R.layout.activity_main)
-        val myCanvas = Earth(this);
-        View.SYSTEM_UI_FLAG_FULLSCREEN.also { myCanvas.systemUiVisibility = it }
-        setContentView(myCanvas);
+        setContentView(R.layout.activity_main)
+
+        //val myCanvas = Kirito(this);
+        //View.SYSTEM_UI_FLAG_FULLSCREEN.also { myCanvas.systemUiVisibility = it }
+        //setContentView(myCanvas);
     }
 }
