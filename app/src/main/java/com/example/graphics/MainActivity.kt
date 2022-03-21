09184810891f17ba4403fd5b4873ff5addddff32
Sawluis <luispineda200504@gmail.com>
2022-03-20 19:53:57 -0600

    Menu (Vuelvo a subirlo)

--- app/src/main/java/com/example/graphics/MainActivity.kt
+++ app/src/main/java/com/example/graphics/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.graphics
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // setContentView(R.layout.activity_main)
-        val myCanvas = Earth(this);
-        View.SYSTEM_UI_FLAG_FULLSCREEN.also { myCanvas.systemUiVisibility = it }
-        setContentView(myCanvas);
+        setContentView(R.layout.activity_main)
+
+        val BotonJugar: Button = findViewById(R.id.btnJugar)
+
+        BotonJugar.setOnClickListener(){
+            try {
+                val myCanvas = Earth(this);
+                View.SYSTEM_UI_FLAG_FULLSCREEN.also { myCanvas.systemUiVisibility = it }
+                setContentView(myCanvas);
+            }catch (e: Exception){}
+        }
     }
 }
